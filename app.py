08a4109a08d71de7336334debd216c0445a84077
Advik-mittal-cfc <smarthelmetatl@gmail.com>
2024-10-28 09:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ _,but,_ = main.columns([1,2,1])
 if but.button("Calculate Your Carbon Footprint!", type="primary"):
     click_element('tab-1')
 
-tab1, tab2, tab3, tab4, tab5 = comps.tabs(["👴 Personal","🚗 Travel","🗑️ Waste","⚡ Energy","💸 Consumption"])
+tab1, tab2, tab3, tab4, tab5 = comps.tabs(["👤 Personal","🚗 Travel","🗑️ Waste","⚡ Energy","💸 Consumption"])
 tab_result,_ = result.tabs([" "," "])
 
 def component():
